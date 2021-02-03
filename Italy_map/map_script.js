26ad43dbc0ed6efbcbed0f3afa52cec2200f7a71
FrancescoArtibani97 <artibani.1743611@studenti.uniroma1.it>
2021-02-03 13:00:45 +0100

    updates on Italy map and menu

--- Italy_map/map_script.js
+++ Italy_map/map_script.js
@@ -1,41 +1,42 @@
 //set width and height of svg shape
-var width = 700,
-    height = 900;
-    var margin = { top: -5, right: -5, bottom: -5, left: -5 }
-//set scale of colours
-var nameRegions = ["Trentino Alto Adige / Südtirol","Piemonte","Valle d'Aosta / Vallée d'Aoste","Liguria","Lombardia","Veneto","Friuli-Venezia Giulia",
-                    "Emilia-Romagna","Toscana","Umbria","Marche","Lazio","Abruzzo","Molise","Campania","Puglia","Basilicata","Calabria","Sicilia","Sardegna"]
+var width = 500,
+    height = 600;
 
-var nameProvinces =[];
-var sumDel = 0; //var for the sum of delicts on the clicked region
-var color = null;
-var numbers = [];
-var reg_population = {};
-var density_crime = 0;
-var selected_crimes = ['strage'];
+var margin = { top: -5, right: -5, bottom: -5, left: -5 }
 
-//retrive population data of provinces and save on a dictionary
-d3.csv("./datasets/dataset_popolazione_italiana/popolazione_italiana(prov+reg).csv", function(data) {
-  for (let index = 0; index < data.length; index++) {
-      reg_population[data[index].Territorio]=data[index].Value;
-  }
-  console.log(reg_population)
-})
+var sumDelPop=0; //var for the sum of crimes divided population
+var sumDel=0 //var for the sum of crimes
+var colorProv = null; //province colour scale
+var colorReg = null; //province colour scale
+var numbers = []; //for set of numbers of delicts of each territory
+var list_crimes = []; //list of all crimes retrieved from dataset (in 'manageCrimesSelection.js' file)
+var count = 0; //count number of swapping among maps 
+var population=0;
 
-//zoom variables
+
+
+
+
+//create leaflet container
+var leafletCont= d3.select('#content').append('div').attr('class', 'leaflet-control-container')
+                                      .append('div').attr('class','leaflet-top leaflet-left')
+                                      .append('div').attr('id','slide').attr('class','leaflet-control-zoom leaflet-bar leaflet-control leaflet-zoom-anim')
+
+
+//zoom 
 let container = d3.selectAll("svg g");
 
 var zoom = d3.zoom()
                 .scaleExtent([1, 10])
                 .on("zoom", zoomed);
 
-    var drag = d3.drag()
+var drag = d3.drag()
                 .subject(function (d) { return d; })
                 .on("start", dragstarted)
                 .on("drag", dragged)
                 .on("end", dragended);
 
-    var slider = d3.select("#slide").append("p").append("input")
+var slider = d3.select("#slide").append("p").append("input")
                 .datum({})
                 .attr("type", "range")
                 .attr("value", zoom.scaleExtent()[0])
@@ -44,148 +45,28 @@ var zoom = d3.zoom()
                 .attr("step", (zoom.scaleExtent()[1] - zoom.scaleExtent()[0]) / 100)
                 .on("input", slided);
 
-    var svg = d3.selectAll("svg")
+var svg = d3.selectAll("svg")
             .attr("transform", "translate(" + margin.left + "," + margin.right + ")")
             .call(zoom);
 
-
-    function zoomed() {
+function zoomed() {
       const currentTransform = d3.event.transform;
       container.attr("transform", currentTransform);
       slider.property("value", currentTransform.k);
-    }
-
-    function dragstarted(d) {
+}
+function dragstarted(d) {
       d3.event.sourceEvent.stopPropagation();
       d3.select(this).classed("dragging", true);
-    }
-
-    function dragged(d) {
+}
+function dragged(d) {
       d3.select(this).attr("cx", d.x = d3.event.x).attr("cy", d.y = d3.event.y);
-    }
-
-    function dragended(d) {
+}
+function dragended(d) {
       d3.select(this).classed("dragging", false);
-    }
-
-    function slided(d) {
-      zoom.scaleTo(d3.select("svg"), d3.select(this).property("value"));
-    }
-//zoom();
-
-
-
-
-
-/*
-//save all possible values of regions delicts (seeing the specific query )
-d3.csv("./dataset_delitti/target.csv", function(data) {
-      regions = d3.nest() //create dictionary on regions name
-               .key(function(d) { return d.Region; })
-               .entries(stocks = data);      
-
-      regions.forEach(function (s){
-          if(nameRegions.includes(s.key)){
-              s.values.forEach(function(d) { 
-                    sumDel = d3.sum(s.values, function(d){ //filter per year and crime types
-                                            //es:   if(d.Year == "2011" & d.Code == "CULPINJU")
-                                              return d.Value;})}
-              )
-              
-              //number of crimes
-              if(d3.select("#computation").node().value== "0" || d3.select("#computation").node().value== "1" ){
-                  numbers.push(sumDel);
-              }
-              //number of crimes/region population
-              else if(d3.select("#computation").node().value == "2"){
-                numbers.push(sumDel/reg_population[s.key]);
-              }
-              //number of crimes/region area
-              else{
-                
-              }
-            
-              sumDel=0;
-          }
-          
-      })
-
-      console.log(numbers)
-      color = d3.scaleLinear()
-                .domain(d3.extent(numbers)) //select min an max of retrived values
-                .range(["yellow","red"]);
-                //"#ffffcc","#ffeda0","#fed976","#feb24c","#fd8d3c","#fc4e2a","#e31a1c","#bd0026","#800026"
-});
-*/
-
-//PROVINCES
-//save all possible values of provinces crimes (seeing the specific query )
-var list_provinces=[]
-d3.csv("./datasets/dataset_delitti/10_years_dat.csv", function(data) {
-  regions = d3.nest() //create dictionary on regions name
-           .key(function(d) {
-              list_provinces.push(d.Territorio); //retrive all teritory from dataset 
-              return d.Territorio; })
-           .entries(stocks = data);    
-  
-
-  list_provinces = removeRegions(removeDuplicates(list_provinces)); //remove duplicates and regions and macro-areas
-  
-
-  regions.forEach(function (s){
-      if(list_provinces.includes(s.key)){
-        
-          s.values.forEach(function(d) { 
-            //console.log(d);
-                sumDel = d3.sum(s.values, function(d){ //filter per year and crime types
-                                        //es:   if(d.Year == "2011" & d.Code == "CULPINJU")
-                    tot_selected_crimes_per_year=0;
-                    //console.log(d);
-                  
-                    if(selected_crimes.length==0){ //if array of crimes is empty then select all crimes
-                        return d.totale;
-                    }
-                    else{ //if some crimes are been selected
-                      for (const [key, value] of Object.entries(d)) {
-                          if(selected_crimes.includes(key)){ //"selected_crimes" is the array that contain all crimes slected by user (by default it start with all crimes)
-                              tot_selected_crimes_per_year+=value;
-                          }
-                      }
-                      return tot_selected_crimes_per_year;
-                    }
-                })
-          })
-          
-          //number of crimes
-          if(d3.select("#computation").node().value== "0" || d3.select("#computation").node().value== "1" ){
-              numbers.push(sumDel);
-            //  console.log(sumDel);
-          }
-
-          
-          //number of crimes/region population
-          else if(d3.select("#computation").node().value == "2"){
-            numbers.push(sumDel/reg_population[s.key.trim()]);
-          }
-          //number of crimes/region area
-          else{
-            
-          }
-        
-          sumDel=0;
-      }
-      
-  })
-
-  console.log(numbers)
-  color = d3.scaleLinear()
-            .domain(d3.extent(numbers)) //select min an max of retrived values
-            .range(["yellow","red"]);
-            //"#ffffcc","#ffeda0","#fed976","#feb24c","#fd8d3c","#fc4e2a","#e31a1c","#bd0026","#800026"
-
-});
-    
-
+}
+function slided(d) {
+      zoom.scaleTo(d3.selectAll("svg"), d3.select(this).property("value"));
+}
 
 //tooltip defined for show name of region when arrive the mouse
 var tooltip = d3.select("#content")
@@ -193,71 +74,63 @@ var tooltip = d3.select("#content")
      .attr("class", "tooltip hidden")
      .attr("id","tooltip");
  
+var selectAllProv = d3.select('.leaflet-top')
+                    //.append('div').attr('class','leaflet-control-container')
+                    //.append('div').attr('class','leaflet-up leaflet-left')
+                    .append('div').attr('id','checkAll').attr('class', 'leaflet-bar leaflet-control')
+                    .text('Select All ')
+                    .append('input')
+                    .attr('type','checkbox').attr( 'id','selectAll')
+                  
+//manage events of selectAllProv
+d3.select("#selectAll").on("change", selectAllTer); //select or unselect all reg/prov
+//---------------------------------------------------------------
+//---------------------------------------------------------------
 
 
-var mapProv = d3.json('./datasets/dataset_mappa_italiana/mappa_italiana_provincie.json', function(data) {
-      createMapProv(data);
+//MENU VARIABLES
+var visualization = '0';
+var computationType=0; //variable that contain the computationType of the moment: =0 =>number of crimes; =1 => num.crimes/population 
+var selectedYears=["2012","2013","2014","2015","2016","2017","2018","2019"]; //variable tha contain the years selected (start with all years selected))
+var selected_crimes = []; //variable that contain crimes selected
+
+//MENU CODE
+
+//compute for number of crimes or num.crimes/population 
+d3.select('#computationCrimes')
+  .on('change', function() {
+    var newData = eval(d3.select(this).property('value'));//0 if choose only number of crimes or 1 if choose num_crimes/pop
+    loadComputationMap(newData);
+    //loadComputationParallelCoordinates(newData); (valerio [menu])
+});
+computeColourScales();//compute colour scales considering only num crimes
+//compute parCoord(); //(valerio [start function]) compute par. coord. considering only num crimes
+
+//visualize for region or province map
+d3.select('#visualization')
+  .on('change', function() {
+    var newData = eval(d3.select(this).property('value'));//0 if choose to vis. for province or 1 if choose to vis for regions
+    visualization=newData; //variable that mantain value 0 or 1 (default setted to 0 => visualize for provinces)
+    loadMap(newData);
+    //loadParallelCoordinates(newData); (valerio [menu]) (must load par. coord. with prov or reg)
+});
+loadMap("0");//Province map
+//loadParallelCoordinates(newData); (valerio [start function])
+
+//Years (valerio=>function to implement is on 'updateSelectedYears' function)
+d3.selectAll(".yearCheckbox").on("change",updateSelectedYears); //update list of selected years ('selectedYears') + map
+
+d3.selectAll(".yearCheckbox").property('checked',true);//start with all years selected
     
-      var g=d3.select('#content g.mapProv');
-      g.selectAll('path')  
-            .on('mouseover',
-              function(){
-                if(d3.select(this).attr("clicked") != 1){
-                  d3.select(this)
-                    .attr('class','white');
-                }
-              }
-            );
-    
-      var g=d3.select('#content g.mapProv');
-      g.selectAll('path')  
-            .on('mouseout',
-              function(){
-                if(d3.select(this).attr("clicked") != 1){
-                  d3.select(this)
-                  .attr('class','greyProv');
-                }
-              tooltip.attr('style', null)
-                  .classed("hidden", true)
-                  .node().innerHTML = null;
-              
-              }
-            );
-    
-})
-
-
-
-
-
-var mapReg = d3.json('./datasets/dataset_mappa_italiana/mappa_italiana_regioni.json', function(data) {
-  createMapReg(data);
-/*
-  var g=d3.select('#content g.mapReg');
-  g.selectAll('path')  
-        .on('mouseover',
-          function(){
-            if(d3.select(this).attr("clicked") != 1){
-              d3.select(this)
-                .attr('class','white');
-            }
-          }
-        );
-
-  var g=d3.select('#content g.mapReg');
-  g.selectAll('path')  
-        .on('mouseout',
-          function(){
-            if(d3.select(this).attr("clicked") != 1){
-              d3.select(this)
-              .attr('class','greyReg');
-            }
-          tooltip.attr('style', null)
-              .classed("hidden", true)
-              .node().innerHTML = null;
-          
-          }
-        );
-*/
-})
-
+//manage years checkboxes
+var expanded = false;
+function showCheckboxes() {
+  var checkboxes = d3.select("#yearCheckboxes");
+  if (!expanded) {
+    checkboxes.node().style.display = "block";
+    expanded = true;
+  } else {
+    checkboxes.node().style.display = "none";
+    expanded = false;
+  }
+}
